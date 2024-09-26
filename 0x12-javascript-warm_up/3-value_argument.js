9d4e3b09b210acbf6941122747d15f64832b1d53
Siyabonga Bukhali <>
2024-09-26 14:55:46 +0200

    Fix permissions and remove use of length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argumentsArr = process.argv.slice(2);
-if (argumentsArr.length === 0) {
+if (argumentsArr[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(argumentsArr[0]);

714ab7b380594a16a27119c8764c5f18d4487439
Siyabonga Bukhali <>
2024-09-26 20:27:53 +0200

    Add statement for multiple arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const argumentsArr = process.argv.slice(2);
 if (argumentsArr.length === 0) {
   console.log('No argument');
-} else {
+} else if (argumentsArr.length === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

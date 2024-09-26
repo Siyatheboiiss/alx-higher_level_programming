acfd0b79f0ff1ad55c27bf3ab9445e8ec265dcea
Siyabonga Bukhali <>
2024-09-26 23:16:00 +0200

    Use the MySQLdb module more

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,21 @@ def main():
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    results = cursor.fetchall()
+    try:
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        results = cursor.fetchall()
 
-    for state in results:
-        print(state)
-
-    cursor.close()
-    db.close()
+        for state in results:
+            print(state)
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
 
 if __name__ == "__main__":
     main()

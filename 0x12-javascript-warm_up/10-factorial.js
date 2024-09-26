5cba352976fbfd8c2621550cf3be934d45b7b20e
Siyabonga Bukhali <>
2024-09-26 19:04:51 +0200

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const param = parseInt(process.argv[2]);
+const factorial = (number) => {
+  if (isNaN(number)) {
+    return 1;
+  }
+  number = parseInt(number);
+  if (number < 0) {
+    return undefined;
+  }
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+};
+
+console.log(factorial(param));

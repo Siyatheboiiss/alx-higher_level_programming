4e902361ba234799b7afb3dd79cee65279c4350c
Siyabonga Bukhali <>
2024-09-27 00:46:58 +0200

    Minor change

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Get the HTTP status code and store it in 'response'
 response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"

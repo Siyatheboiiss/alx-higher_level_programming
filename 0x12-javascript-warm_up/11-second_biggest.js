01cfab7239079326469d8bb960a05f70db07c278
Siyabonga Bukhali <>
2024-09-26 19:35:09 +0200

    Use return instead of console.log

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const listOfIntegers = process.argv.slice(2);
 const sortAndReturnSecondBiggest = (arr) => {
   if (arr.length < 2) {
-    console.log(1);
+    return 1;
   }
   arr.sort((a, b) => b - a);
   return arr[1];

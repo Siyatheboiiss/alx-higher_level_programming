63f084e7dcbc02423ed2930900a85a23db5fb0c0
Siyabonga Bukhali <>
2024-09-26 20:51:46 +0200

    Semistandard, yeah I know.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (this.isValid(w) && this.isValid(h)) {
       this.width = w;
       this.height = h;
     }
   }
 
-  isValid(value) {
+  isValid (value) {
     return Number.isInteger(value) && value > 0;
   }
 }

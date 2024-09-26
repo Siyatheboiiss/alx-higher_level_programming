a0c6f363227beaa3de167f3ab9924592386c8531
Siyabonga Bukhali <>
2024-09-26 19:08:48 +0200

    Use standard functions not arrow functions

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const param = parseInt(process.argv[2]);
-const factorial = (number) => {
+function factorial(number) {
   if (isNaN(number)) {
     return 1;
   }
@@ -12,6 +12,6 @@ const factorial = (number) => {
     return 1;
   }
   return number * factorial(number - 1);
-};
+}
 
 console.log(factorial(param));

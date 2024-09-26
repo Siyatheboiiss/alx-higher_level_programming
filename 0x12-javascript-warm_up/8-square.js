bbb6a78827495cce7211adc8f821e781b963ac27
Siyabonga Bukhali <>
2024-09-26 20:25:49 +0200

    Use single quotes again

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,11 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size) || size <= 0) {
   console.log('Missing size');
 } else {
-    
-    for (let rowIndex = 0; rowIndex < size; rowIndex++) {
-      let rowString = '';
-      for (let colIndex = 0; colIndex < size; colIndex++) {
-        rowString += 'X'; 
-        }
-    console.log(rowString);
+  for (let rowIndex = 0; rowIndex < size; rowIndex++) {
+    let rowString = '';
+    for (let colIndex = 0; colIndex < size; colIndex++) {
+      rowString += 'X';
     }
+    console.log(rowString);
+  }
 }

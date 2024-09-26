514e8a51c904240a23acfeb22081ebc9c1afe22e
Siyabonga Bukhali <>
2024-09-26 14:49:48 +0200

    Fix indentation and redundant semicolon

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argumentsArr = process.argv.slice(2);
-if(argumentsArr.length === 0) {
-    console.log('No argument');
+if (argumentsArr.length === 0) {
+  console.log('No argument');
 } else {
-    console.log('Argument found');
-}; 
+  console.log('Argument found');
+}

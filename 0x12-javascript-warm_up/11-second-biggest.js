623c1574c0cc370187dc51b9eac66f56e5b08d11
Siyabonga Bukhali <>
2024-09-26 19:30:49 +0200

    Second biggest

--- 0x12-javascript-warm_up/11-second-biggest.js
+++ 0x12-javascript-warm_up/11-second-biggest.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const listOfIntegers = process.argv.slice(2);
 const sortAndReturnSecondBiggest = (arr) => {
+  if (arr.length < 2) {
+    console.log(1);
+  }
   arr.sort((a, b) => b - a);
   return arr[1];
 };

408e32dc1a39c658b392b309a8757fd8cedf4078
Siyabonga Bukhali <>
2024-09-26 21:29:06 +0200

    Remove error handling

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,25 +5,18 @@ const [fileA, fileB, fileC] = process.argv.slice(2);
 
 fs.readFile(fileA, 'utf8', (err, dataA) => {
   if (err) {
-    console.error(`Error reading ${fileA}: ${err.message}`);
     return;
   }
 
-  fs.readFile(fileB, "utf8", (err, dataB) => {
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
     if (err) {
-      console.error(`Error reading ${fileB}: ${err.message}`);
       return;
     }
 
-    const content = dataA + dataB;
-
-    fs.writeFile(fileC, content, (err) => {
+    fs.writeFile(fileC, dataA + dataB, (err) => {
       if (err) {
-        console.error(`Error writing to ${fileC}: ${err.message}`);
         return;
       }
-
-      console.log(`Contents of ${fileA} and ${fileB} concatenated to ${fileC}`);
     });
   });
 });

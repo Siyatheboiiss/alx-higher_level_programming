019e94e1f23a62241c0cba7263334dabba7ba4dd
Siyabonga Bukhali <>
2024-09-26 21:04:45 +0200

    Add single quote for strings

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,7 +14,7 @@ class Rectangle {
   print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
-        console.log("X".repeat(this.width));
+        console.log('X'.repeat(this.width));
       }
     }
   }

5234ad357da7afc4d1fba82691c1c9e2f9d2da1e
Siyabonga Bukhali <>
2024-09-26 19:36:03 +0200

    Fix return statement

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const listOfIntegers = process.argv.slice(2);
 const sortAndReturnSecondBiggest = (arr) => {
   if (arr.length < 2) {
-    return 1;
+    return 0;
   }
   arr.sort((a, b) => b - a);
   return arr[1];

5734b7ff0675a0b999624e5c471a95645c987ff3
Siyabonga Bukhali <>
2024-09-27 00:43:29 +0200

    Make it 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # This script tells you the size of the body when a request is made to a page
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the URL and get the size of the response body
 curl -s "$1" | wc -c

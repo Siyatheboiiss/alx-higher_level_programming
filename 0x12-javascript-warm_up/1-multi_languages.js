aa44987b457af3be9724c83bf36bb89b306cb9e0
Siyabonga Bukhali <>
2024-09-26 14:21:33 +0200

    Multi languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const cIsFun = 'C is fun';
+const pythonIsCool = 'Python is cool';
+const jsIsAmazing = 'JavaScript is amazing';
+console.log(`${cIsFun}\n${pythonIsCool}\n${jsIsAmazing}`)

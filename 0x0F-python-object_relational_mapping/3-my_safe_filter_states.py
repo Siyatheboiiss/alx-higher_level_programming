9fc1527f280c5994739b6cbbc5d2cfa4c6bb0770
Siyabonga Bukhali <>
2024-09-27 11:35:29 +0200

    Use single quotes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,11 +4,11 @@ import MySQLdb
 import sys
 
 if __name__ == "main":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
     match = sys.argv[4]
-    c.execute("""SELECT * FROM states WHERE name LIKE %s""", (match,))
+    c.execute('SELECT * FROM states WHERE name LIKE %s', (match,))
     rows = c.fetchall()
     for row in rows:
         print(row)

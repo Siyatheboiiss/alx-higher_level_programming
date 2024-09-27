341205b7d6418c8ff4310f9f385b55b5a11daf7d
Siyabonga Bukhali <>
2024-09-27 12:09:56 +0200

    Final commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script tells you the size of the body when a request is made to a page
+# Get the byte size of the HTTP response header for a given URL.
 curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the HTTP status code and store it in 'response'
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
